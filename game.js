1d25a48032ab546b6b13f0166b033f66f7d82b7e
Temirbek <atabekov.t.a@gmail.com>
2022-06-09 01:02:32 +0600

    second try

--- game.js
+++ game.js
@@ -1,19 +1,18 @@
+import {generate} from './generate.js';
+let a,b,c = generate();
 let k =0;
-let inp_x = createInput('');
-inp_x.position(0, 0);
-inp_x.size(100);
-let inp_y = createInput('');
-inp_y.position(0, 0);
-inp_y.size(100);
-const a = Math.floor(Math.random() * 1000);
-const b = Math.floor(Math.random() * 1000);
-const c = Math.floor(Math.random() * 1000);
 let x_pic,y_pic;
 
 function setup() {
-createCanvas(400, 400);
+createCanvas(800, 800);
 
 FindSolutions()
+let inp_x = createInput('');
+inp_x.position(200, 200);
+inp_x.size(100);
+let inp_y = createInput('');
+inp_y.position(200, 250);
+inp_y.size(100);
 // Create buttons for creating
 // and removing the canvas
 
@@ -26,16 +25,17 @@ function FindSolutions()
 {
     const res = [];
     let x,y;
-    for (let x = 1; x < 1000; x++){
-        if (Number.isInteger(y = (c - a*x)/b)){
+    for (let x = 1; x <= 1000; x++){
+        if (Number.isInteger(y = (c - a*x)/b) && (c - a*x)/b > 0){
             res.push([x,y]);
         }
     }
-    x_pic,y_pic = res[Math.random() * res.length];
+    [x_pic, y_pic] = res[Math.random() * (res.length - 1)];
 }
 
 function keyPressed() {
-    if (keyCode === ENTER) {
+    if (inp_x.value.length == 0 || inp_y.value.length == 0) text("One of the fields is empty", 50, 350);
+    else if (keyCode === ENTER) {
         inp_x.input(CompareEventX);
         inp_y.input(CompareEventY);
     } 
@@ -44,30 +44,30 @@ function keyPressed() {
 function CompareEventX(){
     if (this.value > x_pic)
     {
-        text(">", 50, 150);
+        text(">", 50, 200);
     }
     else if (this.value < x_pic)
     {
-        text("<", 50, 150);
+        text("<", 50, 200);
     }
     else 
     {
-        text("This one is right!", 50, 150);
+        text("This one is right!", 50, 200);
     }
 }  
 
 function CompareEventY(){
     if (this.value > y_pic)
     {
-        text(">", 50, 150);
+        text(">", 50, 250);
     }
     else if (this.value < y_pic)
     {
-        text("<", 50, 150);
+        text("<", 50, 250);
     }
     else 
     {
-        text("This one is right!", 50, 150);
+        text("This one is right!", 50, 250);
     }
 }  
 
@@ -82,5 +82,5 @@ textSize(20);
 
 let eq = a.toString() + "*x+"+b.toString()+"*y="+c.toString();
 
-text("Enter x and y value that satisfies this equation, then press ENTER: " + eq, 50, 150);
+text("Enter x and y value that satisfies this equation, then press ENTER:\n " + eq, 50, 350);
 }

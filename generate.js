f9206f7571feb4f44cd344978be815a93f8ba0af
muratsat <muratsat@users.noreply.github.com>
2022-06-08 21:43:25 +0300

    made equations solvable

--- generate.js
+++ generate.js
@@ -1,7 +1,10 @@
 function generate() {
     const a = Math.floor(Math.random() * 1000);
     const b = Math.floor(Math.random() * 1000);
-    const c = Math.floor(Math.random() * 1000);
+    const x = Math.floor(Math.random() * 1000);
+    const y = Math.floor(Math.random() * 1000);
+
+    const c = a*x + b*y;
     return {
         "a": a,
         "b": b,
